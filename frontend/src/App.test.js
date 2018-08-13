24d7d6d88fba9872871a7ff375365ca2b7ce6264
Marc Farra <marcfarra@gmail.com>
2018-08-13 15:47:55 -0400

    Mock mapbox GL

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/* 
+  Mock Mapbox GL to not render anything
+  because it requires a browser environment 
+  to work.
+*/
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({})
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
