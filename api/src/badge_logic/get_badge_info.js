fe4b3bcc44901f48fec7c2527398b510d8247ba2
Alyssa <alyssadelaine@gmail.com>
2018-10-02 15:35:13 -0400

    Return description and metric type so they can be used on the frontend, and change variable names to be clearer

--- api/src/badge_logic/get_badge_info.js
+++ api/src/badge_logic/get_badge_info.js
@@ -1,25 +1,25 @@
 /**
- * Given a metric and a key to a badge
+ * Given a metricValue and a metricName to a badge
  * calculate the level of that badge and the percentage amount
  * needed to obtain the next badge
  */
 //eslint-disable-next-line no-unused-vars, consistent-return
-module.exports = (metric, key, badge) => {
+module.exports = (metricValue, metricName, badge) => {
   const { tiers, name, id } = badge
   let badgeLevel = 0
 
-  if (metric >= tiers[0] && metric < tiers[1]) {
+  if (metricValue >= tiers[0] && metricValue < tiers[1]) {
     badgeLevel = 1
   }
-  else if (metric >= tiers[1] && metric < tiers[2]) {
+  else if (metricValue >= tiers[1] && metricValue < tiers[2]) {
     badgeLevel = 2
   }
-  else if (metric >= tiers[2]) {
+  else if (metricValue >= tiers[2]) {
     badgeLevel = 3
   }
 
   const nextBadgeLevel = badgeLevel + 1
-  const currentPoints = Number(metric)
+  const currentPoints = Number(metricValue)
   let lastPoints = 0
   let nextPoints = 0
   let percentage = 100
@@ -31,6 +31,8 @@ module.exports = (metric, key, badge) => {
     return {
       name: name,
       category: id,
+      metric: metricName,
+      description: badge.description,
       badgeLevel,
       nextBadgeLevel,
       points: {
