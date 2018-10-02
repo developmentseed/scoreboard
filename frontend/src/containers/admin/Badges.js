b6839f8b2286dd961ab4d448ceba645614cc778a
sethvincent <sethvincent@gmail.com>
2018-10-02 10:32:32 -0700

    add initial container for badges admin

---
+++ frontend/src/containers/admin/Badges.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import api from '../utils/api';
+
+class BadgesAdmin extends Component {
+  constructor () {
+    super();
+    this.state = {}
+  }
+
+  componentDidMount () {}
+
+  render() {}
+}
+
+export default BadgesAdmin;
