e10c7ddb6ffe7ca30985ac49a585f5dea1d2107b
Alyssa <alyssadelaine@gmail.com>
2018-10-04 17:34:23 -0400

    Add test suite

---
+++ api/tests/badges.test.js
@@ -0,0 +1,85 @@
+
+const test = require('ava')
+const request = require('supertest')
+const connection = require('../src/db/connection')
+const app = require('../src/index')
+
+let db
+
+test.before(async () => {
+  db = connection()
+  await db.migrate.latest()
+  await db.seed.run()
+})
+
+test.after.always(async () => {
+  await db.destroy()
+})
+
+test('Getting a badge from the db', async (t) => {
+  const res = await request(app)
+    .get('/scoreboard/api/badges/1')
+    .expect(200)
+  // name should always be included
+  t.true('name' in res.body.records)
+})
+
+test('Pull all badges', async (t) => {
+  const res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  const numBadges = res.body.records.length
+  if (numBadges > 0) {
+    // name should always be included
+    t.true('name' in res.body.records[numBadges - 1])
+  }
+})
+
+test('Inserting a badge into the db', async (t) => {
+  let res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  const numBadges = res.body.records.length
+  await request(app)
+    .post('/scoreboard/api/badges')
+    .send({ name: 'Test Badge', operations: [['>', 'daysTotal', '100']] })
+    .expect(200)
+  res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  const numBadges2 = res.body.records.length
+  t.true(numBadges + 1 === numBadges2)
+  t.true(res.body.records[numBadges - 1].name === 'Test Badge')
+  t.true(res.body.records[numBadges - 1].operations === [['>', 'daysTotal', '100']])
+})
+
+test('Updating a badge in the db', async (t) => {
+  await request(app)
+    .put('/scoreboard/api/badges')
+    .send({
+      name: 'Test Badge Edit',
+      operations: [['>', 'daysTotal', '100'], ['>=', 'daysInRow', '200']]
+    })
+    .expect(200)
+  const res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  const numBadges = res.body.records.length
+  t.true(res.body.records[numBadges - 1].name === 'Test Badge Edit')
+  t.true(res.body.records[numBadges - 1].operations
+    === [['>', 'daysTotal', '100'], ['>=', 'daysInRow', '200']])
+})
+
+test('Deleting a badge from the db', async (t) => {
+  let res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  const numBadges = res.body.records.length
+  await request(app)
+    .delete('/scoreboard/api/badges/', numBadges - 1)
+    .expect(200)
+  res = await request(app)
+    .get('/scoreboard/api/badges')
+    .expect(200)
+  t.true(res.body.records.length === numBadges - 1)
+})
