d85b5635cdd057443772bce1bbf3b53e0c8aa751
Alyssa <alyssadelaine@gmail.com>
2018-10-04 18:01:44 -0400

    Remove unnecessary test, correct URL

--- api/tests/badges.test.js
+++ api/tests/badges.test.js
@@ -47,8 +47,6 @@ test.serial('Inserting a badge into the db', async (t) => {
   res = await request(app)
     .get(`/scoreboard/api/badges/${numBadges + 1}`)
     .expect(200)
-  const numBadges2 = res.body.badges.length
-  t.true(numBadges + 1 === numBadges2)
   t.true(res.body.badges[0].name === 'Test Badge')
   t.true(res.body.badges[0].operations === [['>', 'daysTotal', '100']])
 })
@@ -59,7 +57,7 @@ test.serial('Updating a badge in the db', async (t) => {
     .expect(200)
   const numBadges = res.body.badges.length
   await request(app)
-    .put('/scoreboard/api/badges')
+    .put(`/scoreboard/api/badges/${numBadges}`)
     .send({
       name: 'Test Badge Edit',
       operations: [['>', 'daysTotal', '100'], ['>=', 'daysInRow', '200']]
