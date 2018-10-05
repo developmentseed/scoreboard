1360ca33b2ed55c5186d2ba9861f3d6a62067ea8
Aurora Nou <auroranou@gmail.com>
2018-10-05 15:43:50 -0400

    Make it possible to add several operations

--- frontend/src/containers/admin/Badges.js
+++ frontend/src/containers/admin/Badges.js
@@ -11,11 +11,18 @@ const badgeMetrics = [
   { label: 'Total days mapped', value: 'daysTotal' },
   { label: 'Days mapped in a row', value: 'daysInRow' },
   { label: 'Campaigns participated in', value: 'hashtags' },
-  { label: 'Times using JOSN', value: 'josm' },
+  { label: 'Times using JOSM', value: 'josm' },
   { label: 'Points of interest mapped', value: 'pois' },
   { label: 'New roads mapped (km)', value: 'roadKms' },
   { label: 'Roads modified (km)', value: 'roadKmMods' },
   { label: 'Waterways mapped', value: 'waterways' },
+];
+
+const badgeOperationTypes = [
+  { label: 'More than', value: '>' },
+  { label: 'Fewer than', value: '<' },
+  { label: 'At least', value: '>=' },
+  { label: 'Up to', value: '<=' }
 ];
 
 class BadgesAdmin extends Component {
@@ -24,42 +31,51 @@ class BadgesAdmin extends Component {
 
     this.state = {
       descriptionInput: '',
-      metricInput: {},
+      disableInteraction: false,
       nameInput: '',
       numberInput: '',
-      operationInput: {},
-      badges: []
+      operations: [
+        {
+          metric: '',
+          number: 0,
+          operation: ''
+        }
+      ]
     };
 
+    // API actions
     this.fetchBadges = this.fetchBadges.bind(this);
     this.postBadges = this.postBadges.bind(this);
 
+    // Event handlers
+    this.addAnotherOperationToBadge = this.addAnotherOperationToBadge.bind(this);
     this.handleAddNewBadgeFormSubmit = this.handleAddNewBadgeFormSubmit.bind(this);
     this.handleDescriptionInputChange = this.handleDescriptionInputChange.bind(this);
-    this.handleMetricInputChange = this.handleMetricInputChange.bind(this);
     this.handleNameInputChange = this.handleNameInputChange.bind(this);
-    this.handleNumberInputChange = this.handleNumberInputChange.bind(this);
-    this.handleOperationInputChange = this.handleOperationInputChange.bind(this);
-
-    this.fetchBadges();
+    this.handleOperationChange = this.handleOperationChange.bind(this);
+    this.removeOperationFromBadge = this.removeOperationFromBadge.bind(this);
   }
 
   async fetchBadges() {
     try {
       const res = await api('get', createApiUrl(BADGES_ENDPOINT, {}));
       const { badges } = res.data;
-      this.setState({ badges });
+      console.log(badges);
     } catch (e) {
       console.log('Error fetching badges: ', e);
     }
   }
 
   async postBadges(params) {
+    this.setState({ disableInteraction: true });
     try {
       const res = await api('post', BADGES_ENDPOINT, params);
       console.log(res);
+      this.setState({ disableInteraction: false });
+      this.fetchBadges();
     } catch (e) {
       console.log('Error posting badge: ', e);
+      this.setState({ disableInteraction: false });
     }
   }
 
@@ -93,101 +109,19 @@ class BadgesAdmin extends Component {
         id='form-add-new-badge'
         onSubmit={this.handleAddNewBadgeFormSubmit}
       >
-        <fieldset className='form__column'>
-          <legend className='header--medium'>
-            Badge Details
-          </legend>
-          <div className='form__input-unit'>
-            <label
-              className='form__label'
-              htmlFor='add-new-badge-name'
-            >
-              Custom Badge Name
-            </label>
-            <input
-              id='badge-name'
-              name='badge-name'
-              onChange={this.handleNameInputChange}
-              placeholder='Awesome JOSM'
-              type='text'
-              value={this.state.nameInput}
-            />
-          </div>
-          <div className='form__input-unit'>
-            <label
-              className='form__label'
-              htmlFor='badge-description'
-            >
-              Description
-            </label>
-            <textarea
-              id='badge-description'
-              name='badge-description'
-              maxLength={150}
-              onChange={this.handleDescriptionInputChange}
-              placeholder='Let users know about how this badge works'
-              rows={5}
-              value={this.state.descriptionInput}
-            />
-          </div>
-        </fieldset>
-        <fieldset className='form__column'>
-          <legend className='header--medium'>
-            What do users need to do to achieve this badge?
-          </legend>
-          <div class='form__input-unit form__input-unit--half'>
-            <label
-              className='form__label'
-              htmlFor='badge-operation-type'
-            >
-              Threshold Type
-            </label>
-            <Select
-              id='badge-operation-type'
-              name='badge-operation-type'
-              onChange={this.handleOperationInputChange}
-              options={[
-                { label: 'More than', value: '>' },
-                { label: 'At least', value: '>=' }
-              ]}
-              placeholder="Select how you'll gauge this metric"
-              value={this.state.operationInput}
-            />
-          </div>
-          <div className='form__input-unit form__input-unit--half'>
-            <label
-              className='form__label'
-              htmlFor='badge-metric-number'
-            >
-              Number
-            </label>
-            <input
-              id='badge-metric-number'
-              name='badge-metric-number'
-              onChange={this.handleNumberInputChange}
-              placeholder='50'
-              type='number'
-            />
-          </div>
-          <div className='form__input-unit'>
-            <label
-              className='form__label'
-              htmlFor='badge-metric'
-            >
-              Metric
-            </label>
-            <Select
-              id='badge-metric'
-              name='badge-metric'
-              onChange={this.handleMetricInputChange}
-              options={badgeMetrics}
-              placeholder='Select the metric your badge will measure...'
-              value={this.state.metricInput}
-            />
-          </div>
-        </fieldset>
+        {this.renderBasicDetailsSection()}
+        {this.renderOperationsSection()}
         <div className='form__footer'>
           <button
+            className='button button--secondary'
+            id='add-new-badge-operation-button'
+            onClick={this.addAnotherOperationToBadge}
+            type='button'
+          >
+            Add another condition
+          </button>
+          <button
+            className='button'
             id='add-new-badge-submit-button'
             type='submit'
           >
@@ -196,6 +130,155 @@ class BadgesAdmin extends Component {
         </div>
       </form >
     );
+  }
+
+  renderBasicDetailsSection() {
+    return (
+      <div className='form__section'>
+        <h2 className='header--medium'>Badge Details</h2>
+        <div className='form__input-unit'>
+          <label
+            className='form__label'
+            htmlFor='add-new-badge-name'
+          >
+            Custom Badge Name
+          </label>
+          <input
+            id='badge-name'
+            name='badge-name'
+            onChange={this.handleNameInputChange}
+            placeholder='Awesome JOSM'
+            type='text'
+            value={this.state.nameInput}
+          />
+        </div>
+        <div className='form__input-unit'>
+          <label
+            className='form__label'
+            htmlFor='badge-description'
+          >
+            Description
+          </label>
+          <textarea
+            id='badge-description'
+            name='badge-description'
+            maxLength={150}
+            onChange={this.handleDescriptionInputChange}
+            placeholder='Let users know about how this badge works'
+            rows={5}
+            value={this.state.descriptionInput}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  renderOperationsSection() {
+    return (
+      <div className='form__section'>
+        <h2 className='header--medium'>
+          What do users need to do to achieve this badge?
+      </h2>
+        {this.state.operations.map((op, i) => (
+          <fieldset
+            key={`${i}`}
+            style={{ borderBottom: '1px solid #eee', paddingBottom: '1em' }}
+          >
+            <div className='form__input-unit form__input-unit--half'>
+              <label
+                className='form__label'
+                htmlFor='badge-operation-type'
+              >
+                Condition
+            </label>
+              <Select
+                id='badge-operation-type'
+                name='badge-operation-type'
+                onChange={(e) => this.handleOperationChange(e, i, 'operation')}
+                options={badgeOperationTypes}
+                placeholder="Select how you'll gauge this metric"
+                value={op.operation}
+              />
+            </div>
+            <div className='form__input-unit form__input-unit--half'>
+              <label
+                className='form__label'
+                htmlFor='badge-metric-number'
+              >
+                Number
+            </label>
+              <input
+                id='badge-metric-number'
+                min={0}
+                name='badge-metric-number'
+                onChange={(e) => this.handleOperationChange(e, i, 'number')}
+                placeholder='50'
+                type='number'
+                value={op.number}
+              />
+            </div>
+            <div className='form__input-unit'>
+              <label
+                className='form__label'
+                htmlFor='badge-metric'
+              >
+                Metric
+            </label>
+              <Select
+                id='badge-metric'
+                name='badge-metric'
+                onChange={(e) => this.handleOperationChange(e, i, 'metric')}
+                options={badgeMetrics}
+                placeholder='Select the metric your badge will measure...'
+                value={op.metric}
+              />
+            </div>
+            {i > 0 && (
+
+              <button
+                className='button button--link'
+                id='delete-badge-operation-button'
+                onClick={(e) => this.removeOperationFromBadge(e, i)}
+                type='button'
+              >
+                Remove this condition
+            </button>
+            )}
+          </fieldset>
+        ))}
+      </div>
+    );
+  }
+
+  addAnotherOperationToBadge() {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        operations: [
+          ...prevState.operations,
+          {
+            metric: '',
+            number: 0,
+            operation: ''
+          }
+        ]
+      };
+    });
+  }
+
+  removeOperationFromBadge(e, idx) {
+    e.preventDefault();
+
+    // Can't delete the only set
+    if (idx === 0) {
+      return;
+    }
+
+    if (this.state.operations[idx]) {
+      const nextOperations = [...this.state.operations];
+      nextOperations.splice(idx, 1);
+      this.setState({ operations: nextOperations });
+    }
   }
 
   handleNameInputChange(e) {
@@ -212,22 +295,20 @@ class BadgesAdmin extends Component {
     });
   }
 
-  handleMetricInputChange(newVal) {
-    this.setState({
-      metricInput: newVal
-    });
-  }
+  handleOperationChange(e, idx, keyName) {
+    const { value } = keyName === 'number' ? e.target : e;
+    let targetOperation = this.state.operations[idx];
+    if (!targetOperation) return;
 
-  handleNumberInputChange(e) {
-    const { value } = e.target;
-    this.setState({
-      numberInput: value
-    });
-  }
+    targetOperation = {
+      ...targetOperation,
+      [keyName]: value
+    };
 
-  handleOperationInputChange(newVal) {
-    this.setState({
-      operationInput: newVal
+    this.setState((state) => {
+      return {
+        operations: Object.assign([...state.operations], { [idx]: targetOperation })
+      }
     });
   }
 
@@ -236,24 +317,17 @@ class BadgesAdmin extends Component {
 
     const {
       descriptionInput,
-      metricInput,
       nameInput,
-      numberInput,
-      operationInput,
+      operations,
     } = this.state;
-    const metric = metricInput.value;
-    const operation = operationInput.value;
 
     const params = {
       description: descriptionInput,
       name: nameInput,
-      operations: [[
-        operation,
-        metric,
-        numberInput
-      ]]
+      operations: operations.map(op => [op.operation, op.metric, op.number])
     };
 
+    console.log(params);
     this.postBadges(params);
   }
 }
