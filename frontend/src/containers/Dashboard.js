d1df9b9d18d14ceca996b8c0fead83968d064d8d
sethvincent <sethvincent@gmail.com>
2018-10-08 15:06:46 -0700

    remove unused import from Dashboard.js

--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { connect } from 'unistore/react';
 import { actions } from '../store'
 
-import api from '../utils/api';
 import '../styles/Dashboard.css';
 
 import { UserExtentMap } from '../components/charts';
