6230a22e06df6aac45424026197e5711a5c9cd18
Alyssa <alyssadelaine@gmail.com>
2018-10-17 22:48:57 -0400

    Update test script

--- api/test.sh
+++ api/test.sh
@@ -1,11 +1,9 @@
 if [ -z "$CI" ]; then
     createdb -p 5433 scoreboard_tests
-    cd src/db && node_modules/.bin/knex migrate:latest && cd ../..
-    yarn seed
+    cd src/db && node_modules/.bin/knex migrate:latest && yarn seed && cd ../..
     ava tests/*.test.js
     dropdb  -p 5433 scoreboard_tests
 else
-    node_modules/.bin/knex migrate:latest
-    yarn seed
+    cd src/db && node_modules/.bin/knex migrate:latest && yarn seed && cd ../..
     ava tests/*.test.js
 fi
