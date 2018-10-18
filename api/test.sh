5afb63db5fd1e8e13901d9f8d805ea3189a783e7
Alyssa <alyssadelaine@gmail.com>
2018-10-17 22:29:59 -0400

    knex migrate from correct directory

--- api/test.sh
+++ api/test.sh
@@ -1,6 +1,6 @@
 if [ -z "$CI" ]; then
     createdb -p 5433 scoreboard_tests
-    node_modules/.bin/knex migrate:latest
+    cd src/db && node_modules/.bin/knex migrate:latest && cd ../..
     yarn seed
     ava tests/*.test.js
     dropdb  -p 5433 scoreboard_tests
