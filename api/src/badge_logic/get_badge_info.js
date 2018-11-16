5d1760bb906742cce145c16f83982abfa37b9d28
Alyssa <alyssadelaine@gmail.com>
2018-11-16 11:58:47 -0500

    Create functions for complex date logic

--- api/src/badge_logic/get_badge_info.js
+++ api/src/badge_logic/get_badge_info.js
@@ -1,5 +1,3 @@
-
-
 const metricIndex = 1
 const opIndex = 0
 const valueIndex = 2
@@ -21,20 +19,59 @@ function mapBadgeToTask (badge, x) {
   return map[badge]
 }
 
-function execLogic(op, compValue, requiredValue) {
+function execLogic (op, compValue, requiredValue) {
   let bp = ''
-  if (op[0] === '<') {
-    if (op === '<=') (bp = compValue <= requiredValue)
-    else (bp = compValue < requiredValue)
-  }
-  else if (op[0] === '>') {
-    if (op === '>=') (bp = compValue >= requiredValue)
-    else bp = (compValue > requiredValue)
-  }
-  else if (op === '=') {
-    bp = (compValue === requiredValue)
+  switch (op[0]) {
+    case '<':
+      if (op === '<=') (bp = compValue <= requiredValue)
+      else (bp = compValue < requiredValue)
+      break
+    case '>':
+      if (op === '>=') (bp = compValue >= requiredValue)
+      else bp = (compValue > requiredValue)
+      break
+    case '=':
+      bp = (compValue === requiredValue)
+      break
   }
   return bp
+}
+
+function betweenDates (firstValidDay, lastValidDay, days, today, excluded) {
+  const lastDateSplit = lastValidDay.split('-')
+  const lastValidDate = new Date(lastDateSplit[0], Number(lastDateSplit[1] - 1), lastDateSplit[2])
+  const firstDateSplit = firstValidDay.split('-')
+  const firstValidDate = new Date(firstDateSplit[0],
+    Number(firstDateSplit[1] - 1), firstDateSplit[2])
+  const pass = days.filter(
+    // In this case, `requiredPointValue` will be the list [firstDate, lastDate]
+    (day) => (execLogic('>=', day, firstValidDay) &&
+      execLogic('<=', day, lastValidDay))
+  ).length > 0
+  /* eslint-disable brace-style */
+  if ((lastValidDate < today) || (today < firstValidDate)) {
+    return [ pass, true, '' ]
+  }
+  else {
+    return [ pass, excluded, mapBadgeToTask('allDays', lastValidDay) ]
+  }
+}
+
+function simpleDateComp (requiredDay, days, operator, today, excluded) {
+  // Check whether any days mapped are before, after, or equal to the requested date
+  const pass = days.filter(
+    // In this case, `requiredPointValue` will be a date in string format
+    (day) => execLogic(operator, day, requiredDay)
+  ).length > 0
+  // Only show gettable badges in progress
+  const validDay = requiredDay.split('-')
+  const validDate = new Date(validDay[0], Number(validDay[1] - 1), validDay[2])
+  if (!(execLogic(operator, today, validDate))) {
+    return [ pass, true, '' ]
+  }
+  else if (operator[0] === '<') {
+    return [ pass, excluded, mapBadgeToTask('allDays', requiredDay) ]
+  }
 }
 
 /**
@@ -51,6 +88,7 @@ module.exports = (userMetrics, badge) => {
   let badgeOperationPass = true
   let excludeFromInProgress = false
   let task = ''
+  let dateTaskPhrase = ''
   let metricName = ''
   let currentPointValue = 0
   let requiredPointValue = 0
@@ -58,15 +96,6 @@ module.exports = (userMetrics, badge) => {
   let percentage = 100
   let thisPercentage = 100
 
-  let firstValidDay = ''
-  let firstValidDate = ''
-  let lastValidDay = ''
-  let lastValidDate = ''
-  let lastDateSplit = ''
-  let firstDateSplit = ''
-  let validDate = ''
-  let validDay = ''
-  let dateTaskPhrase = ''
   operations.forEach((badgeOpArray) => {
     const operator = badgeOpArray[opIndex]
     metricName = badgeOpArray[metricIndex]
@@ -74,55 +103,24 @@ module.exports = (userMetrics, badge) => {
     requiredPointValue = badgeOpArray[valueIndex]
     if (metricName === 'campaigns') {
       // Check whether any hashtags match the required one
-      badgeOperationPass = userMetrics[metricName].filter(
+      badgeOperationPass = currentPointValue.filter(
         // In this case, `requiredPointValue` will be the name of a hashtag
         (tag) => execLogic(operator, tag, requiredPointValue)
       ).length > 0
       excludeFromInProgress = true
-    }
-    else if (metricName === 'allDays') {
+    } else if (metricName === 'allDays') {
       const today = new Date()
       if (operator === 'between') {
         // the `between` operator assumes two dates in the order of [firstDate, lastDate]
         // where to earn a badge a user must have mapped on some validDay
         // where firstDate <= validDay <= lastDate
-        firstValidDay = badgeOpArray[valueIndex]
-        lastValidDay = badgeOpArray[valueIndex + 1]
-        badgeOperationPass = userMetrics[metricName].filter(
-          // In this case, `requiredPointValue` will be the list [firstDate, lastDate]
-          (day) => (execLogic('>=', day, firstValidDay)
-            && execLogic('<=', day, lastValidDay))
-        ).length > 0
-        lastDateSplit = lastValidDay.split('-')
-        lastValidDate = new Date(lastDateSplit[0], Number(lastDateSplit[1] - 1), lastDateSplit[2])
-        firstDateSplit = firstValidDay.split('-')
-        firstValidDate = new Date(firstDateSplit[0],
-          Number(firstDateSplit[1] - 1), firstDateSplit[2])
-        /* eslint-disable brace-style */
-        if (lastValidDate < today) { excludeFromInProgress = true }
-        else if (today < firstValidDate) { excludeFromInProgress = true }
-        else {
-          dateTaskPhrase = mapBadgeToTask('allDays', lastValidDay)
-        }
+        [ badgeOperationPass, excludeFromInProgress, dateTaskPhrase ] =
+          betweenDates(badgeOpArray[valueIndex], badgeOpArray[valueIndex + 1], currentPointValue, today, excludeFromInProgress)
+      } else {
+        [ badgeOperationPass, excludeFromInProgress, dateTaskPhrase ] =
+          simpleDateComp(requiredPointValue, currentPointValue, operator, today, excludeFromInProgress)
       }
-      else {
-        // Check whether any days mapped are before, after, or equal to the requested date
-        badgeOperationPass = userMetrics[metricName].filter(
-          // In this case, `requiredPointValue` will be a date in string format
-          (day) => execLogic(operator, day, requiredPointValue)
-        ).length > 0
-        // Only show gettable badges in progress
-        validDay = requiredPointValue.split('-')
-        validDate = new Date(validDay[0], Number(validDay[1] - 1), validDay[2])
-        if (!(execLogic(operator, today, validDate))) {
-          excludeFromInProgress = true
-        }
-        else if (operator[0] === '<') {
-          dateTaskPhrase = mapBadgeToTask('allDays', requiredPointValue)
-        }
-      }
-    }
-    else {
+    } else {
       badgeOperationPass = execLogic(operator, currentPointValue, requiredPointValue)
     }
     if (badgeOperationPass === false) {
@@ -144,7 +142,7 @@ module.exports = (userMetrics, badge) => {
   if (badgePass === true) {
     badgeLevel = 1
   }
-  else { //if (badgeLevel === 0) {
+  else { // if (badgeLevel === 0) {
     if (percentage === 100) { percentage = 0 } // Simple unearned date badges will show 0 progress
     task = `${Math.floor(percentage)}% of the way to earning this badge. ${task}`
   }
