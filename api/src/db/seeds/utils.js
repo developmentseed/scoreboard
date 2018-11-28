78bc9652834cf57874cec4f85d8b8a9343cd98d2
Alyssa <alyssadelaine@gmail.com>
2018-11-28 16:42:01 -0500

    Randomize last_edit

--- api/src/db/seeds/utils.js
+++ api/src/db/seeds/utils.js
@@ -6,14 +6,15 @@ const countries = require('i18n-iso-countries')
 function generateUsers (n, knex) {
   const countryIds = Object.keys(countries.getNames('en'))
   const users = []
+  const today = new Date()
   for (let i = 0; i < n; i += 1) {
     const user = {
       osm_id: 100000000 + i,
       full_name: `test${i} McTest`,
       country: countryIds[parseInt(Math.random() * (countryIds.length - 1))],
-      edit_count: Math.floor(Math.random() * 100000000)
+      edit_count: Math.floor(Math.random() * 100000000),
+      last_edit: new Date(today.setDate(today.getDate() - 365 * Math.random()))
     }
-
     users.push(user)
   }
 
