98d6f58c88659daf0b637b347e792613de973f98
scisco <me@scisco.io>
2018-11-29 12:38:21 -0500

    store sessions in db in production

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const boom = require('express-boom')
 const session = require('express-session')
+const KnexSessionStore = require('connect-session-knex')(session)
 const compression = require('compression')
 const YAML = require('yamljs')
 const swaggerUi = require('swagger-ui-express')
@@ -14,8 +15,9 @@ const pckg = require('../../package.json')
 const router = require('./routes')
 
 const app = express()
+const connection = require('./db/connection')
 
-const { SESSION_SECRET } = require('./config')
+const { SESSION_SECRET, NODE_ENV } = require('./config')
 const { passport, authRouter } = require('./passport')
 
 /**
@@ -25,10 +27,34 @@ const { passport, authRouter } = require('./passport')
 const swaggerDocument = YAML.load(path.join(__dirname, '..', '/docs/api.yml'))
 swaggerDocument.info.version = pckg.version
 
+/**
+ * Session
+ */
+
+let sessionConfig = {
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}
+
+if (NODE_ENV === 'production') {
+  const store = new KnexSessionStore({
+    knex: connection()
+  })
+
+  sessionConfig = {
+    secret: SESSION_SECRET,
+    cookie: {
+      maxAge: 10000 // ten seconds, for testing
+    },
+    store: store
+  }
+}
+
 app.use(bodyParser.json())
 app.use(compression())
 app.use(boom())
-app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: true }))
+app.use(session(sessionConfig))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use('/auth', authRouter)
