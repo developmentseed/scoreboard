f575d08b818fc12440925490f8a9ec5f5869c5a4
Alyssa <alyssadelaine@gmail.com>
2018-12-05 09:11:17 -0500

    Add Countries to header

--- pages/_app.js
+++ pages/_app.js
@@ -87,6 +87,7 @@ class Layout extends React.Component {
                 <li className='logo'><Link href='/'><a>ScoreBoard</a></Link></li>
                 <li><Link href='/campaigns'><a>Campaigns</a></Link></li>
                 <li><Link href='/users'><a>Users</a></Link></li>
+                <li><Link href='/countries'><a>Countries</a></Link></li>
                 <li><Link href='/about'><a>About</a></Link></li>
               </ul>
               {
