0892d8fd354fdb4ea9e4f4204148ae3afebd1ba2
scisco <me@scisco.io>
2018-12-07 09:07:42 -0500

    remove countries table

--- api/src/db/migrations/20181204163810_countries.js
+++ api/src/db/migrations/20181204163810_countries.js
@@ -1,25 +1,16 @@
 exports.up = async function (knex, Promise) {
   try {
-    await knex.schema.createTable('countries', table => {
-      table.increments('id')
-      table.string('alpha2')
-      table.string('name')
-      table.integer('edit_count').defaultTo(0)
-      table.timestamps()
-      table.unique('alpha2')
-    })
     await knex.schema.createTable('user_country_edits', table => {
       table.increments('id')
-      table.integer('country_id').references('countries.id').notNull().onDelete('cascade')
+      table.string('country_name')
       table.integer('user_id').references('users.id').onDelete('cascade')
       table.integer('edit_count').defaultTo(0)
       table.timestamps()
-      table.unique(['user_id', 'country_id'])
+      table.unique(['user_id', 'country_name'])
     })
   } catch (e) {
     console.error(e)
     try {
-      await knex.schema.dropTable('countries')
       await knex.schema.dropTable('user_country_edits')
     } catch (e) {
       console.error(e)
@@ -30,11 +21,6 @@ exports.up = async function (knex, Promise) {
 exports.down = async function (knex, Promise) {
   try {
     await knex.schema.dropTable('user_country_edits')
-  } catch (e) {
-    console.error(e)
-  }
-  try {
-    await knex.schema.dropTable('countries')
   } catch (e) {
     console.error(e)
   }
