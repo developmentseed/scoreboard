7a49dfc366371d8cf1ef1eec672ad391ef1137a5
Alyssa <alyssadelaine@gmail.com>
2018-12-10 11:32:10 -0500

    Return empty object if country does not exist to pass test

--- api/src/utils/countryGeometry.js
+++ api/src/utils/countryGeometry.js
@@ -1,10 +1,14 @@
 const countriesGeoJSON = require('./countries.geojson')
 
 function getCountry (countryName) {
-  const countryProperties = countriesGeoJSON.countryGeoJSON.features.find((countryArr) => {
-    return countryArr.properties.ADMIN === countryName
-  })
-  return countryProperties.geometry
+  try {
+    const countryProperties = countriesGeoJSON.countryGeoJSON.features.find((countryArr) => {
+      return countryArr.properties.ADMIN === countryName
+    })
+    return countryProperties.geometry
+  } catch (e){
+    return {}
+  }
 }
 
 module.exports = {
