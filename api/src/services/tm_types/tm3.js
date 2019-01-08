9242065bb765afa1ca08d242336856450508e8e6
Marc Farra <marcfarra@gmail.com>
2019-01-08 00:31:01 -0500

    Add mapResult geometry for cards

--- api/src/services/tm_types/tm3.js
+++ api/src/services/tm_types/tm3.js
@@ -4,6 +4,22 @@ const limit = require('p-limit')(5)
 const extractCampaignHashtag = require('../../utils/extractCampaignHashtag')
 
 /**
+ * @param {FeatureCollection} fc
+ * @returns {Map[ProjectId, Feature]}
+ * Takes the feature collection of mapResults from the projects
+ * of the TM3 api and builds a reverse index of project ids to geometries
+ */
+function buildMapResultsIndex (fc) {
+  let features = {}
+  for (let i = 0; i < fc.features.length; i++) {
+    let feature = fc.features[i]
+    let projectId = feature.properties.projectId
+    features[projectId] = feature
+  }
+  return features
+}
+
+/**
  * Methods to grab data from tasking manager version 3
  */
 class TM3API {
@@ -23,17 +39,32 @@ class TM3API {
       headers: { 'Accept-Language': 'en-US,en;q=0.9' }
     })
     let json = JSON.parse(firstResp)
+    let mapResultsIndex = buildMapResultsIndex(json.mapResults);
+
     concat(records, json.results)
 
     let numPages = json.pagination.pages
     let promises = []
     for (let i = 2; i < numPages; i++) {
       promises.push(limit(() => rp({
-        uri: `${this.url}/api/v1/project/search${i}`,
+        uri: `${this.url}/api/v1/project/search?page=${i}`,
         headers: { 'Accept-Language': 'en-US,en;q=0.9' }
       })))
     }
-    return promises
+    return Promise.all(promises).then(responses => {
+      let projects = []
+
+      // Combine mapResults with records
+      responses.forEach(response => {
+        let results = JSON.parse(response).results
+        results.forEach(project => {
+          project.geometry = mapResultsIndex[project.projectId]
+          projects.push(project)
+        })
+      })
+
+      return projects
+    })
   }
 
   getProject (id) {
@@ -47,9 +78,7 @@ class TM3API {
   }
 
   toDBObjects (records) {
-    const sqlObjects = records.map(async (feature) => {
-      const rp = await this.getProject(feature.projectId)
-      const geometry = JSON.parse(rp).areaOfInterest
+    const sqlObjects = records.map(feature => {
       const {
         created: created_at, last_update: updated_at // eslint-disable-line camelcase
       } = feature
@@ -59,7 +88,7 @@ class TM3API {
         campaign_hashtag: mainHashtag,
         created_at,
         updated_at,
-        geometry,
+        geometry: feature.geometry,
         name: feature.name,
         description: feature.shortDescription,
         validated: feature.percentValidated,
