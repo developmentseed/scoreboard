3a04557d240fe52d5dee687281180ae41e12f6ba
LanesGood <lane@developmentseed.org>
2019-01-25 12:25:15 -0500

    Initial menu handler function working - WIP

--- pages/_app.js
+++ pages/_app.js
@@ -42,22 +42,22 @@ function Footer (props) {
       <div>
         {
           !props.loggedIn
-            ? (<div class='banner banner__signup'>
-              <div class='row'>
-                <div class='banner--content'>
-                  <h2 class='header--xlarge'>Earn a spot on the board</h2>
+            ? (<div className='banner banner__signup'>
+              <div className='row'>
+                <div className='banner--content'>
+                  <h2 className='header--xlarge'>Earn a spot on the board</h2>
                   <p>Join other mappers and track your progress. Earn badges for edits you’ve made and campaigns you've helped complete. Share your contributions to the global mapping ecosystem.</p>
-                  <a href={join(APP_URL_FINAL, '/auth/openstreetmap')} class='link--large'>Sign up with {projectName}</a>
+                  <a href={join(APP_URL_FINAL, '/auth/openstreetmap')} className='link--large'>Sign up with {projectName}</a>
                 </div>
               </div>
             </div>
             )
 
             : (
-              <div class='banner banner__badges'>
-                <div class='row'>
-                  <div class='banner--content'>
-                    <h2 class='header--xlarge'>Map to Earn Badges</h2>
+              <div className='banner banner__badges'>
+                <div className='row'>
+                  <div className='banner--content'>
+                    <h2 className='header--xlarge'>Map to Earn Badges</h2>
                     <p>Track your best work. Earn badges for edits you’ve made and campaigns you've helped complete. Share your progress and contribution to the global mapping ecosystem. </p>
                   </div>
                 </div>
@@ -94,10 +94,14 @@ class Layout extends React.Component {
   constructor () {
     super()
     this.state = {
-      menuVisible: false
+      menuVisible: false,
+      removeClass: false
     }
     this.handleMenuClick = this.handleMenuClick.bind(this)
     this.handleOutsideClick = this.handleOutsideClick.bind(this)
+  }
+  toggle () {
+    this.setState({ removeClass: !this.state.removeClass })
   }
 
   componentDidMount () {
@@ -132,8 +136,11 @@ class Layout extends React.Component {
 
   render () {
     const { authenticatedUser, children } = this.props
-    const { loggedIn, osm, account } = authenticatedUser
-
+    const { loggedIn, osm, account, name } = authenticatedUser
+    let menuClass = ['nav--left', 'mobile-hidden']
+    if (this.state.removeClass) {
+      menuClass.pop('mobile-hidden')
+    }
     return (
       <div className='App'>
         <Head>
@@ -144,10 +151,10 @@ class Layout extends React.Component {
           <div className='row'>
             <nav>
               <ul className='nav--mobile'>
-                <li className='nav--icons mobile-only' ref={node => { this.navButton = node }} onClick={this.handleMenuClick}><img src={menuIcon} alt='Menu icon' /></li>
+                <li className='nav--icons mobile-only'><img src={menuIcon} alt='Menu icon' onClick={this.toggle.bind(this)} /></li>
                 <li className='logo'><Link href='/'><a>ScoreBoard</a></Link></li>
               </ul>
-              <ul className='nav--left'>
+              <ul className={menuClass.join(' ')}>
                 <li><NavLink href='/campaigns'>Campaigns</NavLink></li>
                 <li><NavLink href='/users'>Users</NavLink></li>
                 <li><NavLink href='/teams'>Teams</NavLink></li>
