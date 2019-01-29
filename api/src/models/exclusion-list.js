10e303a52477956ad8574828da63eae3da785474
Marc Farra <marcfarra@gmail.com>
2019-01-29 18:49:57 -0500

    Add update method

--- api/src/models/exclusion-list.js
+++ api/src/models/exclusion-list.js
@@ -32,8 +32,22 @@ function remove (user_id) {
   return db('exclusion_list').where('user_id', user_id).del()
 }
 
+/**
+ * Update the exclusion list
+ * @param {Array[ids]} newlist - new exclusion list
+ */
+function update (newlist) {
+  return db.transaction(async trx => {
+    await trx('exclusion_list').truncate()
+    return trx('exclusion_list')
+      .insert(newlist.map(id => ({ 'user_id': id })))
+      .returning('user_id')
+  })
+}
+
 module.exports = {
   list,
   add,
-  remove
+  remove,
+  update
 }
