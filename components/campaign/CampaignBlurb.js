536c16c1c7365d912f3a39e8b7ebe1d6fdb8c654
Alyssa <alyssadelaine@gmail.com>
2019-02-05 17:42:32 -0500

    Only campaigns receive data in form with plural coastlines in variable names

--- components/campaign/CampaignBlurb.js
+++ components/campaign/CampaignBlurb.js
@@ -7,10 +7,10 @@ export default function Blurb ({
   buildings_add,
   poi_add,
   km_waterways_add,
-  km_coastline_add,
-  km_coastline_mod
+  km_coastlines_add,
+  km_coastlines_mod
 }) {
   return <h2 className='header--small width--shortened list--block'>
-    <mark>{users.length}</mark> mappers, mapping <mark>{formatKm(km_roads_add)}</mark> of roads, <mark>{formatDecimal(buildings_add)}</mark> buildings, <mark>{formatDecimal(poi_add)}</mark> Points of Interest, <mark>{formatKm(km_coastline_add + km_coastline_mod)}</mark> of coastlines, and <mark>{formatKm(km_waterways_add)}</mark> of waterways.
+    <mark>{users.length}</mark> mappers, mapping <mark>{formatKm(km_roads_add)}</mark> of roads, <mark>{formatDecimal(buildings_add)}</mark> buildings, <mark>{formatDecimal(poi_add)}</mark> Points of Interest, <mark>{formatKm(km_coastlines_add + km_coastlines_mod)}</mark> of coastlines, and <mark>{formatKm(km_waterways_add)}</mark> of waterways.
   </h2>
 }
