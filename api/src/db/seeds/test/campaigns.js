bdfa3ed9107e7f313e98aa503fd79ad2c4b3c618
Marc Farra <marcfarra@gmail.com>
2019-02-06 16:24:10 -0500

    Add TM2 seed

--- api/src/db/seeds/test/campaigns.js
+++ api/src/db/seeds/test/campaigns.js
@@ -8,23 +8,30 @@ exports.seed = async (knex) => {
    * We create a tm proxy for tapes and fill the database with real
    * tasking manager data
    */
+  const tm2proxy = http.createServer(yakbak('https://tasks.openstreetmap.id', {
+    dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
+  }))
+
   const tm3proxy = http.createServer(yakbak('https://tasks.openstreetmap.us', {
     dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
   }))
 
   await new Promise((resolve, reject) => {
-    tm3proxy.listen(4850, async () => {
-      try {
-        await knex('campaigns').del()
-        await knex('taskers').del()
+    tm2proxy.listen(4851, async () => {
+      tm3proxy.listen(4850, async () => {
+        try {
+          await knex('campaigns').del()
+          await knex('taskers').del()
 
-        // Add a tasking manager
-        await knex('taskers').insert({ type: 'tm3', url: 'http://localhost:4850', name: 'test tm' })
-        await tmWorker()
-        resolve()
-      } catch (e) {
-        reject(e)
-      }
+          // Add tasking managers
+          await knex('taskers').insert({ type: 'tm3', url: 'http://localhost:4850', name: 'test tm3' })
+          await knex('taskers').insert({ type: 'tm2', url: 'http://localhost:4851', name: 'test tm2' })
+          await tmWorker()
+          resolve()
+        } catch (e) {
+          reject(e)
+        }
+      })
     })
-  }).then(() => tm3proxy.close())
+  }).then(() => tm3proxy.close()).then(() => tm2proxy.close())
 }
