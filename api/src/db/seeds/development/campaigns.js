bb78b0b841205a3ae62e9ee149709f14df4c0181
Marc Farra <marcfarra@gmail.com>
2019-03-11 15:10:26 -0400

    add campaigns seed that reads from tasking managers

---
+++ api/src/db/seeds/development/campaigns.js
@@ -0,0 +1,38 @@
+const yakbak = require('yakbak')
+const path = require('path')
+const http = require('http')
+const tmWorker = require('../../../tm_clock')
+
+exports.seed = async (knex) => {
+  /**
+   * We create a tm proxy for tapes and fill the database with real
+   * tasking manager data
+   */
+  const tm3proxy = http.createServer(yakbak('https://tasks.openstreetmap.us', {
+    dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
+  }))
+
+  await new Promise((resolve, reject) => {
+    tm3proxy.listen(4850, async () => {
+      try {
+        await knex('campaigns').del()
+        await knex('taskers').del()
+
+        // Add tasking managers
+        await knex('taskers').insert({ type: 'tm3', url: 'http://localhost:4850', name: 'test tm3' })
+        await tmWorker()
+
+        // Make all the hashtags predictable
+        const campaigns = await knex('campaigns').select()
+        const fixedCampaigns = campaigns.map(c => knex('campaigns').where('id', c.id).update({
+          campaign_hashtag: `project-${c.tm_id}`
+        }))
+        await Promise.all(fixedCampaigns)
+
+        resolve()
+      } catch (e) {
+        reject(e)
+      }
+    })
+  }).then(() => tm3proxy.close())
+}
