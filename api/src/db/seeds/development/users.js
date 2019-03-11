8b46a0ad27d4d1bc48783612eda9e28e85a094e3
Marc Farra <marcfarra@gmail.com>
2019-03-11 15:10:43 -0400

    add users clock to seeds, so no need to run clocks in development

--- api/src/db/seeds/development/users.js
+++ api/src/db/seeds/development/users.js
@@ -1,4 +1,6 @@
 const generateUsers = require('../utils').generateUsers
+const usersClock = require('../../../users_clock')
 
 exports.seed = (knex) => knex('users').del() // delete entries
   .then(() => knex('users').insert(generateUsers(30, knex)))
+  .then(usersClock)
