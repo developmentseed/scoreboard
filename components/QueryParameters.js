e4b1b64eb6427bdac753ed951264d59bad3abc4f
Marc Farra <marcfarra@gmail.com>
2019-03-13 18:37:15 -0400

    add multiple query parameter support

--- components/QueryParameters.js
+++ components/QueryParameters.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
 import qs from 'query-string'
-import { zipObj, map, prop } from 'ramda'
+import { zipObj, map, prop, isNil } from 'ramda'
+
+const emptyParam = () => ({ key: null, value: null })
 
 class QueryParameters extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      params: [{ key: '', value: '' }]
+      params: [emptyParam()]
     }
 
     this.update = this.update.bind(this)
     this.onChange = this.onChange.bind(this)
+    this.addNewParam = this.addNewParam.bind(this)
   }
 
   update (idx, field) {
@@ -26,13 +29,26 @@ class QueryParameters extends Component {
     }
   }
 
-  onChange () {
+  addNewParam () {
     let params = this.state.params
+    params.push(emptyParam())
+    this.setState({
+      params
+    })
+  }
 
-    // [{ key, value }, { key, value }] => {key:value, key:value}
-    let paramBuilder = zipObj(map(prop('key'), params), map(prop('value'), params))
+  onChange () {
+    // Filter out not entirely filled out params
+    let params = this.state.params.filter(pair =>
+      !(isNil(pair.key) || isNil(pair.value) || (pair.key.length === 0) || (pair.value.length === 0))
+    )
 
-    this.props.onChange(qs.stringify(paramBuilder))
+    if (params.length) {
+      // [{ key, value }, { key, value }] => {key:value, key:value}
+      let paramBuilder = zipObj(map(prop('key'), params), map(prop('value'), params))
+
+      this.props.onChange(qs.stringify(paramBuilder))
+    }
   }
 
   render () {
@@ -42,12 +58,20 @@ class QueryParameters extends Component {
       <div>
         {
           params.map((p, idx) => (
-            <div key={idx}>
+            <div key={idx} style={{ marginBottom: '5px' }}>
               <input type='text' placeholder='Key' style={{ width: '40%', margin: 'auto' }} onChange={this.update(idx, 'key')} />
               <input type='text' placeholder='Value' style={{ width: '40%', marginLeft: '4px' }} onChange={this.update(idx, 'value')} />
             </div>
           ))
         }
+        <br />
+        <input
+          type='button'
+          className='button'
+          id='add-new-param-button'
+          onClick={this.addNewParam}
+          value='Add new parameter'
+        />
       </div>
     )
   }
