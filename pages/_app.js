5e790b9f0832fce407c0c5b9ba7c846823efc14c
Marc Farra <marcfarra@gmail.com>
2019-03-13 21:05:44 -0400

    Update pages/_app.js
    Co-Authored-By: LanesGood <goodman.ls@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -47,7 +47,6 @@ const NavLink = withRouter(({ children, router, href }) => {
 const { publicRuntimeConfig } = getConfig()
 const VersionNumber = publicRuntimeConfig.versionNumber
 
-console.log(VersionNumber)
 
 function Footer (props) {
   return (
