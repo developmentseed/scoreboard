fa63ff24337709b3780174c3872c3437e4060fee
Marc Farra <marcfarra@gmail.com>
2019-03-18 14:10:26 -0400

    Add component name for debugging

--- components/UserTable.js
+++ components/UserTable.js
@@ -3,35 +3,37 @@ import Link from './Link'
 import { sortBy, prop } from 'ramda'
 import { formatDecimal } from '../lib/utils/format'
 
-export default (props) => (
-  <div className='widget'>
-    <table>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Name</th>
-          <th>{props.editType}</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          sortBy(prop('edits'), props.users)
-            .reverse()
-            .map((user, idx) => (
-              <tr key={user.uid}>
-                <td>{idx + 1}</td>
-                <td>
-                  <Link href={`/users/${user.uid}`}>
-                    <a className='link--normal' >
-                      {user.name}
-                    </a>
-                  </Link>
-                </td>
-                <td>{formatDecimal(user.edits)}</td>
-              </tr>
-            ))
-        }
-      </tbody>
-    </table>
-  </div>
-)
+export default function UserTable (props) {
+  return (
+    <div className='widget'>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>{props.editType}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            sortBy(prop('edits'), props.users)
+              .reverse()
+              .map((user, idx) => (
+                <tr key={user.uid}>
+                  <td>{idx + 1}</td>
+                  <td>
+                    <Link href={`/users/${user.uid}`}>
+                      <a className='link--normal' >
+                        {user.name}
+                      </a>
+                    </Link>
+                  </td>
+                  <td>{formatDecimal(user.edits)}</td>
+                </tr>
+              ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
