7b156549989e3c14460b26899db1fc6bb9594d2f
Alyssa <alyssadelaine@gmail.com>
2019-04-12 15:41:26 -0400

    Parameterize TM name in NotLoggedIn component

--- components/NotLoggedIn.js
+++ components/NotLoggedIn.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from './Link'
 
 export default ({ message }) => {
+  const tmName = process.env.TM_NAME || 'OSM'
   return (
     <div>
       <section>
@@ -9,7 +10,7 @@ export default ({ message }) => {
           <h2 className='header--large'>You are not logged in!</h2>
           <p>
             <Link href='/auth/openstreetmap'>
-              <a className='link--large'>{message || 'Log in with your OSM account'}</a>
+              <a className='link--large'>{message || `Log in with your ${tmName} account`}</a>
             </Link>
           </p>
         </div>
