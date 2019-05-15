adc67eec09dbc9725c0b8d8110f104cfc5615335
Marc Farra <marcfarra@gmail.com>
2019-05-15 17:34:32 -0400

    add campaign reset

--- pages/campaigns.js
+++ pages/campaigns.js
@@ -49,6 +49,7 @@ export class Campaigns extends Component {
   }
 
   componentDidMount () {
+    this.props.resetCampaign()
     if (!this.props.campaignSearchResults || !Object.keys(this.props.campaignSearchResults.records).length) {
       this.props.handleCampaignsPageChange(1)
     }
@@ -129,4 +130,4 @@ export class Campaigns extends Component {
   }
 };
 
-export default connect(['campaigns', 'campaignSearchResults'], actions)(Campaigns)
+export default connect(['campaigns', 'campaignSearchResults', 'campaign'], actions)(Campaigns)
