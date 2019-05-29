89adbf42de91762cc3e8a6c344278111ad5b274b
Marc Farra <marcfarra@gmail.com>
2019-05-29 18:33:22 -0400

    Fix cascade delete

--- api/src/db/migrations/20190430222835_teams_access_tokens.js
+++ api/src/db/migrations/20190430222835_teams_access_tokens.js
@@ -7,7 +7,7 @@ exports.up = async function (knex) {
 
     await knex.schema.createTable('teams_access_tokens', t => {
       t.increments('id').primary()
-      t.integer('osm_id').references('users.osm_id')
+      t.integer('osm_id').references('users.osm_id').onDelete('CASCADE')
       t.string('access_token')
       t.string('refresh_token')
       t.string('expires_in')
