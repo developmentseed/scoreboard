eaf96b952774753a4e4379ee94aee0c1ffcceb87
Alyssa <alyssadelaine@gmail.com>
2019-05-29 16:39:34 -0400

    Campaign Table displays changesets, not edits

--- components/campaign/CampaignTable.js
+++ components/campaign/CampaignTable.js
@@ -21,7 +21,7 @@ export default function CampaignTable (props) {
             <th>Points of Interest</th>
             <th>Coastlines (Km)</th>
             <th>Waterways (Km)</th>
-            <th>Edits</th>
+            <th>Changesets</th>
           </tr>
         </thead>
         <tbody>
