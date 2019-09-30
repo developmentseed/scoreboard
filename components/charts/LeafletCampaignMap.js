20bd15c56280f1edeaf5980c56127fc455aa93ea
Necoline <necoline@developmentseed.org>
2019-09-30 16:58:45 -0400

    removes fragment

--- components/charts/LeafletCampaignMap.js
+++ components/charts/LeafletCampaignMap.js
@@ -1,30 +1,27 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import centerOfMass from '@turf/center-of-mass'
-import { LoadingState } from '../common/LoadingState'
 
 class CampaignMap extends Component {
   render () {
     if (!this.props.feature.id && this.props.feature.type === 'Feature') {
-      return <LoadingState />
+      return <span>loading map ...</span>
     }
 
     const center = centerOfMass(this.props.feature)
 
     return (
-      <React.Fragment>
-        <Map center={center.geometry.coordinates.reverse()} zoom={6} >
-          <TileLayer url='https://api.mapbox.com/styles/v1/devseed/cjqpb3z440t302smfnewsl6vb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q' />
-          <GeoJSON data={this.props.feature} style={() => {
-            return {
-              color: '#4FCA9C',
-              weight: 1,
-              fillColor: '#4FCA9C',
-              fillOpacity: 0.7
-            }
-          }} />
-        </Map>
-      </React.Fragment>
+      <Map center={center.geometry.coordinates.reverse()} zoom={6} >
+        <TileLayer url='https://api.mapbox.com/styles/v1/devseed/cjqpb3z440t302smfnewsl6vb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q' />
+        <GeoJSON data={this.props.feature} style={() => {
+          return {
+            color: '#4FCA9C',
+            weight: 1,
+            fillColor: '#4FCA9C',
+            fillOpacity: 0.7
+          }
+        }} />
+      </Map>
     )
   }
 }
