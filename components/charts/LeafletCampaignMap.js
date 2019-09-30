f926542f8b43a969a39ade5fbdc5ce19f184fb85
Necoline <necoline@developmentseed.org>
2019-09-30 14:36:05 -0400

    adds in new loading state

--- components/charts/LeafletCampaignMap.js
+++ components/charts/LeafletCampaignMap.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import centerOfMass from '@turf/center-of-mass'
-import { showGlobalLoading, hideGlobalLoading, GlobalLoading } from '../common/LoadingState'
+import { LoadingState } from '../common/LoadingState'
 
 class CampaignMap extends Component {
   render () {
-    if (this.props.feature.id) {
-      // return <span>loading map ...</span>
-      showGlobalLoading(20)
+    if (!this.props.feature.id && this.props.feature.type === 'Feature') {
+      return <LoadingState />
     }
     const center = centerOfMass(this.props.feature)
 
     return (
       <React.Fragment>
-        <GlobalLoading />
         <Map center={center.geometry.coordinates.reverse()} zoom={6} >
           <TileLayer url='https://api.mapbox.com/styles/v1/devseed/cjqpb3z440t302smfnewsl6vb/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q' />
           <GeoJSON data={this.props.feature} style={() => {
