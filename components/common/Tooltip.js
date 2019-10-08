898d8bc01f07152653119a7b1b9412c2c8e7cf2e
Necoline <necoline@developmentseed.org>
2019-10-08 17:05:42 -0400

    imports svg instead of using background img

--- components/common/Tooltip.js
+++ components/common/Tooltip.js
@@ -1,10 +1,13 @@
+import React from 'react'
+import InfoIcon from '../../static/circle-information'
+
 export const Tooltip = props => {
   return (
     <>
       <div className='tooltip-container'>
         {props.children}
         <div className='tooltip'>
-          <i className='tooltip-button' />
+          <InfoIcon className='tooltip-button' />
           <span className='tooltip-info'>{props.description}</span>
         </div>
       </div>
@@ -16,5 +19,5 @@ export const TooltipDescriptions = {
   ROADS: 'kilometers of roads added or modified',
   RAILWAYS: 'kilometers of railways added or modified',
   COASTLINES: 'kilometers of coastlin added or modified',
-  WATERWAYS: 'kilometers of waterways added or modified',
+  WATERWAYS: 'kilometers of waterways added or modified'
 }
