d2e8dc3a56a31aa26fe15d59d61281b6824e67da
sethvincent <sethvincent@gmail.com>
2019-10-14 12:22:04 -0700

    revise tests

--- api/tests/services/osmesadb.test.js
+++ api/tests/services/osmesadb.test.js
@@ -5,19 +5,19 @@ const osmesa = require('../../src/services/osmesa')
 const test = require('ava')
 
 test.serial('get country', async t => {
-  const country = await osmesa.getCountry('GIN')
+  const country = await osmesa.getCountry('BEL')
   console.log('country', country)
-  t.true(country && country.name === 'Guinea')
+  t.true(country && country.name === 'Belgium')
 })
 
 test.serial('get user', async t => {
-  const user = await osmesa.getUser(1)
+  const user = await osmesa.getUser(19239)
   console.log('user', user)
-  t.true(user && user.name === 'user_1')
+  t.true(user && user.name === 'ebrelsford')
 })
 
 test.serial('get campaign', async t => {
-  const campaign = await osmesa.getCampaign(1)
+  const campaign = await osmesa.getCampaign(71)
   console.log('campaign', campaign)
   t.truthy(campaign && campaign.tag)
 })
