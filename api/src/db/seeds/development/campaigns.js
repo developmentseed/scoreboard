419cec08a58f2cb1ec5994e697a2d8fb6471b7c0
Marc Farra <marcfarra@gmail.com>
2019-10-17 15:19:26 -0400

    Add fabricator seeds

--- api/src/db/seeds/development/campaigns.js
+++ api/src/db/seeds/development/campaigns.js
@@ -13,50 +13,38 @@ exports.seed = async (knex) => {
     dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
   }))
 
-  const tm2proxy = http.createServer(yakbak('https://tasks.openstreetmap.id', {
-    dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
-  }))
-
   await new Promise((resolve, reject) => {
     tm3proxy.listen(4850, async () => {
-      tm2proxy.listen(4851, async () => {
-        try {
-          await knex('campaigns').del()
-          await knex('taskers').del()
+      try {
+        await knex('campaigns').del()
+        await knex('taskers').del()
 
-          // Add tasking managers
-          await knex('taskers').insert({
-            type: 'tm3',
-            url: 'http://tasks.openstreetmap.us',
-            name: 'test tm3',
-            options: {
-              proxy: 'http://localhost:4850'
-            }
-          })
-          await knex('taskers').insert({
-            type: 'tm2',
-            url: 'http://tasks.openstreetmap.id',
-            name: 'test tm2',
-            options: {
-              proxy: 'http://localhost:4851'
-            }
-          })
-          await tmWorker()
+        // Add tasking managers
+        await knex('taskers').insert({
+          type: 'tm3',
+          url: 'http://tasks.openstreetmap.us',
+          name: 'test tm3',
+          options: {
+            proxy: 'http://localhost:4850'
+          }
+        })
+        await tmWorker()
 
+        /*
           // Make all the hashtags predictable
-          const campaigns = await knex('campaigns').select()
-          const fixedCampaigns = campaigns.map(c => knex('campaigns').where('id', c.id).update({
-            campaign_hashtag: `project-${c.tasker_id}-${c.tm_id}`,
-            status: chance.pickone(['ARCHIVED', 'DRAFT', 'PUBLISHED'])
-          }))
+        const campaigns = await knex('campaigns').select()
+        const fixedCampaigns = campaigns.map(c => knex('campaigns').where('id', c.id).update({
+          campaign_hashtag: `project-${c.tasker_id}-${c.tm_id}`,
+          status: chance.pickone(['ARCHIVED', 'DRAFT', 'PUBLISHED'])
+        }))
 
-          await Promise.all(fixedCampaigns)
+        await Promise.all(fixedCampaigns)
+        */
 
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
-      })
+        resolve()
+      } catch (e) {
+        reject(e)
+      }
     })
-  }).then(() => tm3proxy.close()).then(() => tm2proxy.close())
+  }).then(() => tm3proxy.close())
 }
