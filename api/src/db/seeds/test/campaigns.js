83deea243d3dce389ca342bd9748d4686562e48e
Marc Farra <marcfarra@gmail.com>
2019-10-21 14:25:07 -0400

    Fix test seeds

--- api/src/db/seeds/test/campaigns.js
+++ api/src/db/seeds/test/campaigns.js
@@ -9,50 +9,36 @@ exports.seed = async (knex) => {
    * We create a tm proxy for tapes and fill the database with real
    * tasking manager data
    */
-  const tm2proxy = http.createServer(yakbak('https://tasks.openstreetmap.id', {
-    dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
-  }))
-
   const tm3proxy = http.createServer(yakbak('https://tasks.openstreetmap.us', {
     dirname: path.join(__dirname, '..', '..', '..', '..', 'tests', 'tapes')
   }))
 
   await new Promise((resolve, reject) => {
-    tm2proxy.listen(4851, async () => {
-      tm3proxy.listen(4850, async () => {
-        try {
-          await knex('campaigns').del()
-          await knex('taskers').del()
+    tm3proxy.listen(4850, async () => {
+      try {
+        await knex('campaigns').del()
+        await knex('taskers').del()
 
-          // Add tasking managers
-          await knex('taskers').insert({
-            type: 'tm3',
-            url: 'http://localhost:4850',
-            options: {
-              proxy: 'http://localhost:4850'
-            },
-            name: 'test tm3'
-          })
-          await knex('taskers').insert({
-            type: 'tm2',
-            url: 'http://localhost:4851',
-            options: {
-              proxy: 'http://localhost:4851'
-            },
-            name: 'test tm2'
-          })
-          await tmWorker()
+        // Add tasking managers
+        await knex('taskers').insert({
+          type: 'tm3',
+          url: 'http://tasks.openstreetmap.us',
+          name: 'test tm3',
+          options: {
+            proxy: 'http://localhost:4850'
+          }
+        })
+        await tmWorker()
 
-          // change some of the campaign statuses
-          const campaigns = await knex('campaigns').select()
-          await Promise.all(campaigns.map(c => knex('campaigns').where('id', c.id).update({
-            status: chance.pickone(['ARCHIVED', 'DRAFT', 'PUBLISHED'])
-          })))
-          resolve()
-        } catch (e) {
-          reject(e)
-        }
-      })
+        const campaigns = await knex('campaigns').select()
+        await Promise.all(campaigns.map(c => knex('campaigns').where('id', c.id).update({
+          status: chance.pickone(['ARCHIVED', 'DRAFT', 'PUBLISHED'])
+        })))
+
+        resolve()
+      } catch (e) {
+        reject(e)
+      }
     })
-  }).then(() => tm3proxy.close()).then(() => tm2proxy.close())
+  }).then(() => tm3proxy.close())
 }
