0ed81183f7d4b5c6f0a6725937be9083299d2834
Marc Farra <marcfarra@gmail.com>
2019-10-21 17:03:27 -0400

    Fix services tests

--- api/tests/services/osmesadb.test.js
+++ api/tests/services/osmesadb.test.js
@@ -5,25 +5,21 @@ const osmesa = require('../../src/services/osmesa')
 const test = require('ava')
 
 test.serial('get country', async t => {
-  const country = await osmesa.getCountry('BEL')
-  console.log('country', country)
-  t.true(country && country.name === 'Belgium')
+  const country = await osmesa.getCountry('AGO')
+  t.true(country && country.name === 'Angola')
 })
 
 test.serial('get user', async t => {
   const user = await osmesa.getUser(19239)
-  console.log('user', user)
   t.true(user && user.name === 'ebrelsford')
 })
 
 test.serial('get campaign', async t => {
-  const campaign = await osmesa.getCampaign(71)
-  console.log('campaign', campaign)
+  const campaign = await osmesa.getCampaign('osmus-tasks-153')
   t.truthy(campaign && campaign.tag)
 })
 
 test.serial('get updates', async t => {
   const lastUpdated = await osmesa.getUpdates()
-  console.log('lastUpdated', lastUpdated)
   t.truthy(lastUpdated)
 })
