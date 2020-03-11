e0aa3ad8dce69bf6a49a8ec8e2c3f1053a843e6f
Lane Goodman <Lane@developmentseed.org>
2020-03-11 15:09:33 -0400

    Add countries sort recordQuery to users API route

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -99,6 +99,12 @@ async function stats (req, res) {
       case 'Alphabetical Z-A':
         recordQuery = recordQuery.orderBy('full_name', 'desc')
         break
+      case 'Countries A-Z':
+        recordQuery = recordQuery.orderBy('country', 'asc')
+        break
+      case 'Countries Z-A':
+        recordQuery = recordQuery.orderBy('country', 'desc')
+        break
       default: // Most total edits
         recordQuery = recordQuery.orderByRaw('edit_count desc, full_name  NULLS LAST')
         break
