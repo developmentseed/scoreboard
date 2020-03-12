85724b864bf5239af7a0d44ec954e7cdad1817a7
Alex G Rice <agr@developmentseed.org>
2020-03-12 09:20:24 -0600

    Update centos/deploy.sh
    Based on the last centos deployment, which we did manually, here are
    some improvements:
    
    - add missing npm run build step
    - add variable and echo prompt for which git tag to use
    - use `bash -e` so script will stop if something fails
    
    Other than that, this deploy script looks fine and we should use it on
    next deployment.

--- deployment/centos/deploy.sh
+++ deployment/centos/deploy.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/bin/bash -e
+
+# change this to the current semver tag, e.g. v1.8.0
+GIT_TAG=master
+
+echo "using git tag or branch: ${GIT_TAG}"
+echo
 
 cd ~
 rm -rf scoreboard
 git clone https://github.com/developmentseed/scoreboard.git
 cd scoreboard
-git checkout master
+git checkout ${GIT_TAG}
 
 # installation
 npm install
 npm run migrate
+npm run build
 
 # move to nginx folder
 cd ../..
