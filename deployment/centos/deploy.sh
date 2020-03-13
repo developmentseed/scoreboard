9bf280381a2b356d01d26cf34ee3e65a32b8940c
Alex G Rice <agr@developmentseed.org>
2020-03-13 16:38:34 -0600

    Improve directory and path usage.
    - don't cd into unknown relative path (../..)
    
    - use iso 8601 yyyy-mm-dd date string for backup directories.
    
    - put repeated path into a variable at top of script (NGINX_PATH)

--- deployment/centos/deploy.sh
+++ deployment/centos/deploy.sh
@@ -10,26 +10,32 @@
 #
 
 GIT_TAG=${1:-master}
+NGINX_PATH=/var/www
 
 echo "using git tag or branch: ${GIT_TAG}"
 echo
 
 cd ~
+
+# find the tag or branch
 rm -rf scoreboard
 git clone https://github.com/developmentseed/scoreboard.git
-cd scoreboard
+pushd scoreboard
 git checkout ${GIT_TAG}
 
-# installation
+# install & build steps
 yarn install
 yarn run migrate
 yarn run build
+popd
 
-# move to nginx folder
-cd ../..
-suffix=$(date +%s)
-sudo mv /var/www/scoreboard /var/www/scoreboard_$suffix || echo 'this is the first deployment'
-sudo mv scoreboard /var/www/
+# update nginx directory
+datestamp=$(date +%F)
+if [ -d "${NGINX_PATH}/scoreboard" ]; then
+    # backup previous deployment
+    sudo mv -v ${NGINX_PATH}/scoreboard ${NGINX_PATH}/scoreboard_${datestamp}
+fi
+sudo mv -v scoreboard ${NGINX_PATH}
 
 # restart services
 sudo systemctl restart scoreboard-api
