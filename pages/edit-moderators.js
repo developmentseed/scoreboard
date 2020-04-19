216cdf98465d5f566d92c5c8af718823fdef0154
Marc Farra <marcfarra@gmail.com>
2020-04-19 01:20:28 -0400

    Add page to change moderator list

---
+++ pages/edit-moderators.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import NotLoggedIn from '../components/NotLoggedIn'
+import { LoadingState } from '../components/common/LoadingState'
+import { connect } from 'unistore/react'
+import { actions } from '../lib/store'
+import Table from '../components/common/Table'
+import { includes, prop } from 'ramda'
+
+const tableSchema = {
+  'headers': {
+    'name': { type: 'string', accessor: 'full_name' },
+    'role': { type: 'string', accessor: 'role' },
+    'button': { type: 'string', accessor: 'button' }
+  },
+  columnOrder: ['name', 'role', 'button'],
+  displaysTooltip: []
+}
+
+export function EditTeamModerators (props) {
+  const [loading, setLoading] = useState(true)
+
+  // On load get the user
+  useEffect(() => {
+    props.getAuthenticatedUser()
+      .then(() => props.getTeam(props.id))
+      .then(() => setLoading(false))
+  }, [])
+
+  if (loading || !props.team) {
+    return <LoadingState />
+  }
+  const { authenticatedUser } = props
+
+  if (!authenticatedUser.loggedIn) {
+    return <NotLoggedIn />
+  }
+
+  const { users, moderators, id: teamId } = props.team
+  const moderatorIds = moderators.map(prop('osm_id'))
+
+  const rows = users.map(u => {
+    const isUserModerator = includes(u.osm_id, moderatorIds)
+    return {
+      full_name: u.full_name,
+      role: isUserModerator ? 'moderator' : 'member',
+      button: isUserModerator
+        ? <button style={{ 'padding': '5px' }} className='button' onClick={() => props.removeModerator({ id: teamId, osmId: u.osm_id })} >Remove Moderator</button>
+        : <button style={{ 'padding': '5px' }} className='button' onClick={() => props.assignModerator({ id: teamId, osmId: u.osm_id })} >Add Moderator</button>
+    }
+  })
+
+  return (
+    <div className='admin'>
+      <section>
+        <div className='row widget-container'>
+          <div className='widget-25'>
+            <h2 className='header--large'>Edit team</h2>
+          </div>
+          <div className='widget-75'>
+            <div>
+              <h1 className='header--xlarge'>Edit Moderators</h1>
+              <div className='widget'>
+                <Table tableSchema={tableSchema} data={rows} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+const Page = connect(['authenticatedUser', 'teams', 'team'], actions)(EditTeamModerators)
+
+Page.getInitialProps = async ({ query }) => {
+  const { id } = query
+  return { id }
+}
+
+export default Page
