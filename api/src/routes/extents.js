956e462a29a89d88b0d17b3811cf86d9d3c4f4f7
Alex G Rice <agr@developmentseed.org>
2020-08-14 17:26:27 -0600

    Fix wrong http error code when s3 fetch of .mvt fails.
    * res.boom.serverError() was not a function, so use the s3 statuscode and the boom library to
    return a more relevant api response.
    
    * upgrade express-boom to get the boomify() function.
    
    * wrap the osmesa.tiles() call with some additional error handling.

--- api/src/routes/extents.js
+++ api/src/routes/extents.js
@@ -1,24 +1,42 @@
 const osmesa = require('../services/osmesa')
 
 /**
+ * For error handling the response cannot have the vector tiles response headers
+ *
+ * @param res
+ */
+function revertHeaders (res) {
+  res.set('Content-Type', 'application/json; charset=utf-8')
+  res.set('Content-Encoding', undefined)
+}
+
+/**
  * User Extents route
  * request is of the form `/api/extents/user/test/:z/:x/:y.mvt'
  * it should be rerouted to the s3 path `s3://s3prefix/user/test/:z/:x/:y.mvt`
  */
 async function userExtent (req, res) {
-  const p = req.params
-  res.set('Content-Type', 'application/vnd.mapbox-vector-tile')
-  res.set('Content-Encoding', 'gzip')
-  osmesa.tiles(`user/${p.user}`, p.z, p.x, p.y)
-    .on('error', (err) => {
-      if (err.code === 'NoSuchKey') {
-        res.boom.notFound()
-      } else {
+  try {
+    const p = req.params
+    res.set('Content-Type', 'application/vnd.mapbox-vector-tile')
+    res.set('Content-Encoding', 'gzip')
+    osmesa.tiles(`user/${p.user}`, p.z, p.x, p.y)
+      .on('error', (err) => {
         console.error(err)
-        res.boom.serverError()
-      }
-    })
-    .pipe(res)
+        revertHeaders(res)
+        // the error is from s3, so use it's http status code & message
+        const { message, statusCode } = err
+        res.boom.boomify(err, { message, statusCode })
+      })
+      .pipe(res)
+  } catch (err) {
+    console.error(err)
+    revertHeaders(res)
+    return Promise.resolve(res.boom.boomify(err, {
+      statusCode: 500,
+      message: 'unhandled error in /api/extents/user'
+    }))
+  }
 }
 
 module.exports = {
