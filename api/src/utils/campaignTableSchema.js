2a83ac0000809a4f625c2939d632c47317b2ebe7
Jeevan Farias <jeevan@developmentseed.org>
2020-08-19 14:32:26 -0400

    Add campaign table schema file

---
+++ api/src/utils/campaignTableSchema.js
@@ -0,0 +1,97 @@
+const osmesaUserStatSchema = {
+  'headers': {
+    'name': { type: 'namelink', accessor: 'name' },
+    'country': { type: 'string', accessor: 'country' },
+    'roads': { type: 'number', accessor: 'km_roads_add_mod' },
+    'buildings': { type: 'number', accessor: 'buildings_add_mod' },
+    'poi': { type: 'number', accessor: 'poi_add_mod' },
+    'railways': { type: 'number', accessor: 'km_railways_add_mod' },
+    'coastlines': { type: 'number', accessor: 'km_coastlines_add_mod' },
+    'waterways': { type: 'number', accessor: 'km_waterways_add_mod' },
+    'changesets': { type: 'number', accessor: 'changeset_count' },
+    'edits': { type: 'number', accessor: 'edit_count' }
+  },
+  'columnOrder': [
+    'name',
+    'country',
+    'roads',
+    'buildings',
+    'poi',
+    'railways',
+    'coastlines',
+    'waterways',
+    'changesets',
+    'edits'
+  ],
+  'displaysTooltip': [
+    'name',
+    'buildings',
+    'poi',
+    'changesets',
+    'edits'
+  ]
+
+}
+const maprouletteChallengeSchema = {
+  'headers': {
+    'fixed': { type: 'number', accessor: 'fixed' },
+    'falsePositive': { type: 'number', accessor: 'falsePositive' },
+    'skipped': { type: 'number', accessor: 'skipped' },
+    'deleted': { type: 'number', accessor: 'deleted' },
+    'alreadyFixed': { type: 'number', accessor: 'alreadyFixed' },
+    'tooHard': { type: 'number', accessor: 'tooHard' },
+    'answered': { type: 'number', accessor: 'answered' },
+    'validated': { type: 'number', accessor: 'validated' },
+    'disabled': { type: 'number', accessor: 'disabled' },
+    'avgTimeSpent': { type: 'number', accessor: 'avgTimeSpent' }
+  },
+  columnOrder: [
+    'fixed',
+    'falsePositive',
+    'skipped',
+    'deleted',
+    'alreadyFixed',
+    'tooHard',
+    'answered',
+    'validated',
+    'disabled',
+    'avgTimeSpent'
+  ],
+  'displaysTooltip': [
+    'fixed',
+    'falsePositive',
+    'skipped',
+    'deleted',
+    'alreadyFixed',
+    'tooHard',
+    'answered',
+    'validated',
+    'disabled',
+    'avgTimeSpent'
+  ]
+
+}
+const maprouletteUserStatSchema = {
+  'headers': {
+    'name': { type: 'namelink', accessor: 'name' },
+    'rank': { type: 'number', accessor: 'rank' },
+    'score': { type: 'number', accessor: 'score' }
+  },
+  columnOrder: [
+    'name',
+    'rank',
+    'score'
+  ],
+  'displaysTooltip': [
+    'name',
+    'rank',
+    'score'
+  ]
+
+}
+
+module.exports = {
+  osmesaUserStatSchema,
+  maprouletteUserStatSchema,
+  maprouletteChallengeSchema
+}
