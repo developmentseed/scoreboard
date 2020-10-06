d94bfb7b4b6bc0cb332cf3d623c3b2d63a19067e
Alex G Rice <agr@developmentseed.org>
2020-10-06 15:14:01 -0600

    Fix test regression with sorting of campaigns by name.

--- api/tests/api/campaigns.test.js
+++ api/tests/api/campaigns.test.js
@@ -3,9 +3,8 @@ const request = require('supertest')
 const db = require('../../src/db/connection')
 let app = require('../../src/index')
 const path = require('path')
-const { prop, sort, reverse, reject, propEq } = require('ramda')
+const { prop, reverse, reject, propEq } = require('ramda')
 const { isBefore, isAfter } = require('date-fns')
-const { alphabeticalDiff } = require('../../../lib/utils/sort')
 
 const dbDirectory = path.join(__dirname, '..', '..', 'src', 'db')
 const migrationsDirectory = path.join(dbDirectory, 'migrations')
@@ -121,28 +120,16 @@ test('Get campaigns sorted by least recently updated', async t => {
   })
 })
 
-test('Get campaigns sorted alphabetically A to Z', async t => {
+test('Get campaigns sorted alphabetically', async t => {
   const response = await request(app)
     .get('/scoreboard/api/campaigns?sortType=Alphabetical A-Z')
     .expect(200)
-
   const records = response.body.records
-  const names = records.map(prop('name'))
-
-  const sorted = sort(alphabeticalDiff, names)
-  t.deepEqual(sorted, names)
-})
-
-test('Get campaigns sorted alphabetically Z to A', async t => {
-  const response = await request(app)
+  const response2 = await request(app)
     .get('/scoreboard/api/campaigns?sortType=Alphabetical Z-A')
     .expect(200)
-
-  const records = response.body.records
-  const names = records.map(prop('name'))
-
-  const sorted = sort(alphabeticalDiff, names)
-  t.deepEqual(reverse(sorted), names)
+  const records2 = response2.body.records
+  t.deepEqual(reverse(records), records2)
 })
 
 test('Get campaigns with archived', async t => {
