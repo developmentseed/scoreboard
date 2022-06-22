63f201aec26a766e175fd230a9a15e0d33d223ba
maxgrossman <maxgrossman@users.noreply.github.com>
2022-06-22 15:22:30 -0400

    found nivo version that gets us symlog

--- components/charts/TimeSeriesEditsChart.js
+++ components/charts/TimeSeriesEditsChart.js
@@ -10,22 +10,6 @@ const chartCrosswalk = {
   'km_coastlines_add_mod': 5
 }
 
-// builds log scale inclusive of largest statistic
-function getLog10Scale (maxInteger) {
-  const linearScale = []
-  const logScale = {}
-
-  let i = 0
-  let next = 0
-  while (next < maxInteger) {
-    next = Math.pow(10, i)
-    linearScale.push(i)
-    logScale[i] = next
-    ++i
-  }
-  return [linearScale, logScale]
-}
-
 export default function TimeSeriesEditsChart ({ userData }) {
   const { keys, data } = userData.reduce((chartData, userData) => {
     chartData.keys[userData.name] = true
@@ -49,39 +33,17 @@ export default function TimeSeriesEditsChart ({ userData }) {
   })
 
   const userKeys = Object.keys(keys)
-  let maxInteger = 0
-
-  for (let d of data) {
-    for (const k of userKeys) {
-      maxInteger += d[k] || 0
-      d[k + '_log'] = d[k]
-      d[k] = Math.log10(d[k])
-      if (d[k] < 0) d[k] = 0 // cut off nums > 0 for now.
-    }
-  }
-
-  const [linearScale, logScale] = getLog10Scale(maxInteger)
-
   return (
     (userData && userData.length)
       ? <ResponsiveBar
         width={800}
         data={data}
-        maxValue={linearScale[linearScale.length - 1]}
         enableLabel={false}
         keys={userKeys}
         indexBy='type'
         margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
         padding={0.3}
-        valueScale={{ type: 'linear' }}
-        gridYValues={linearScale}
-        axisLeft={{
-          tickValues: linearScale,
-          format: (v) => logScale[v]
-        }}
-        tooltip={(d) => {
-          return <div>{`${d.data[d.id + '_log'].toLocaleString('en-US')} ${d.indexValue}`}</div>
-        }}
+        valueScale={{ type: 'symlog' }}
         legends={[
           {
             anchor: 'bottom-right',
